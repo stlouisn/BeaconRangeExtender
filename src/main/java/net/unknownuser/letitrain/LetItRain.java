1f07b0c55094eceb11c9d036e665a685c8472661
UnknownUser95 <luluklzde@gmail.com>
2023-12-30 21:23:10 +0100

    keeping weather can now be toggled

--- src/main/java/net/unknownuser/letitrain/LetItRain.java
+++ src/main/java/net/unknownuser/letitrain/LetItRain.java
@@ -1,6 +1,7 @@
 package net.unknownuser.letitrain;
 
 import net.fabricmc.api.*;
+import net.fabricmc.fabric.api.command.v2.*;
 import net.fabricmc.fabric.api.entity.event.v1.*;
 import net.minecraft.text.*;
 import net.minecraft.world.*;
@@ -8,18 +9,25 @@ import org.slf4j.*;
 
 import java.util.*;
 
-public class LetItRain implements ModInitializer {
-    public static final Logger LOGGER = LoggerFactory.getLogger("letitrain");
+import static net.minecraft.server.command.CommandManager.*;
 
-	float rainGradient = 0f;
-	float thunderGradient = 0f;
-	boolean isRaining = false;
+public class LetItRain implements ModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("LetItRain");
+
+	protected boolean enabled = true;
+	protected float rainGradient = 0f;
+	protected float thunderGradient = 0f;
+	protected boolean isRaining = false;
 
 	@Override
 	public void onInitialize() {
+		registerSleepHandlers();
+		registerCommandHandler();
+	}
 
+	private void registerSleepHandlers() {
 		EntitySleepEvents.START_SLEEPING.register((entity, sleepingPos) -> {
-			if(!entity.isPlayer()) {
+			if (!enabled && !entity.isPlayer()) {
 				return;
 			}
 
@@ -30,11 +38,11 @@ public class LetItRain implements ModInitializer {
 		});
 
 		EntitySleepEvents.STOP_SLEEPING.register((entity, sleepingPos) -> {
-			final World world = entity.getWorld();
-
-			if(!entity.isPlayer() || world.getRegistryKey() != World.OVERWORLD) {
+			if (!enabled && !entity.isPlayer() && entity.getWorld().getRegistryKey() == World.OVERWORLD) {
 				return;
 			}
+
+			final World world = entity.getWorld();
 
 			// without delay, restoring the weather is ignored
 			new Timer().schedule(new TimerTask() {
@@ -46,6 +54,36 @@ public class LetItRain implements ModInitializer {
 				}
 			}, 10);
 		});
+	}
 
+	protected void registerCommandHandler() {
+		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> dispatcher.register(literal("sleepweather")
+				.then(literal("keep")
+				.executes(context -> {
+					if (!enabled) {
+						enabled = true;
+						LOGGER.info("sleep now keeps weather state");
+						context.getSource().sendMessage(Text.literal("weather will now be kept when sleeping"));
+					} else {
+						context.getSource().sendMessage(Text.literal("weather is already kept"));
+					}
+					return 1;
+				}))
+				.then(literal("reset")
+				.executes(context -> {
+					if (enabled) {
+						enabled = false;
+						LOGGER.info("sleep now resets weather state");
+						context.getSource().sendMessage(Text.literal("weather will now reset when sleeping"));
+					} else {
+						context.getSource().sendMessage(Text.literal("weather already resets"));
+					}
+					return 1;
+				}))
+				.executes(context -> {
+					context.getSource().sendMessage(Text.literal("weather " + (enabled ? "is kept" : "resets") + " when sleeping"));
+					return 1;
+				})
+		));
 	}
 }
